e88b04a192ade04da53f5b410cfa64854279d8b5
Ade Rowbotham <aderowbotham@gmail.com>
2018-01-31 15:57:53 +0000

    version 1.1.0
    - fixed mixed quotes (code style improvement)
    - removed custom message
    - added minified version
    
    custom warning messages are no longer possible, see https://stackoverflow.com/questions/276660/how-can-i-override-the-onbeforeunload-dialog-and-replace-it-with-my-own

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,7 @@
 (function(){
 
-  'use strict';
+  "use strict";
 
-  angular.module('app', ['ngWarnOnUnload']);
+  angular.module("app", ["ngWarnOnUnload"]);
 
 }());
--- dist/ng-warn-on-unload.js
+++ dist/ng-warn-on-unload.js
@@ -2,22 +2,24 @@
 
   "use strict";
 
-  angular.module('ngWarnOnUnload', []).directive('ngWarnOnUnload', ["$window", function($window) {
+  angular.module("ngWarnOnUnload", []).directive("ngWarnOnUnload", ["$window", function($window) {
 
     return {
-      ngWarnOnUnload: '=',
+      ngWarnOnUnload: "=",
       link: function(scope, elem, attrs) {
 
         if(!attrs.ngWarnOnUnload){
           throw Error("The ngWarnOnUnload attribute requires a value");
         }
 
-        var warnMessage = attrs.ngWarnOnUnloadMsg ? attrs.ngWarnOnUnloadMsg : "You have unsaved changes.";
+        // @NOTE custom warning messages are no longer possible for security reasons
+        // see https://stackoverflow.com/questions/276660/how-can-i-override-the-onbeforeunload-dialog-and-replace-it-with-my-own
+        /// for this reason we simply return true if the user is to be warned (previously we returned the value of attribute ng-warn-on-unload-msg)
 
         $window.onbeforeunload = function(){
           var warn = scope.$eval(attrs.ngWarnOnUnload);
           if(warn){
-            return warnMessage;
+            return true;
           }
         };
 
